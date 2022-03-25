206b896f11869da805039474c3dbde0ed4a55a7c
Sanjarbek <ismatovsanjarbek@yandex.ru>
2022-03-25 21:01:32 +0500

    updated

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,18 +1,19 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faChevronRight,
-  faChevronLeft,
+  faMagnifyingGlass,
+  faSpinner,
 } from "@fortawesome/free-solid-svg-icons";
 import { Fetch } from "./api/api";
 function Profile() {
   const [items, setItems] = useState();
-
+  const [progress, setProgress] = useState({
+    visibility: "hidden",
+  });
   let api = Fetch();
   const set = () => {
     api.then((data) => {
       List(data);
-      console.log(data);
     });
   };
   let obj;
@@ -28,12 +29,23 @@ function Profile() {
 
     setItems(obj);
   };
+
   return (
     <>
-      <button className="btn btn-primary" onClick={set}>
-        Fetch
+      <button
+        className="btn btn-primary"
+        onClick={() => {
+          set();
+          setProgress({ visibility: "visible" });
+          setTimeout(() => {
+            setProgress({ visibility: "hidden" });
+          }, 1000);
+        }}
+      >
+        <FontAwesomeIcon icon={faMagnifyingGlass} />
       </button>
       <ul>{items ? items : ""}</ul>
+      <img style={progress} className="loading" src="./loading.png" />
     </>
   );
 }
